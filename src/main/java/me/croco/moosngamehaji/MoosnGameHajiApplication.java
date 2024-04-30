98055394b8090c547e96da184b4cbbd68436d558
cr0c0 <hyde69ciel@gmail.com>
2024-04-30 13:31:49 +0900

    #240430 스프링부트 초기구성

---
+++ src/main/java/me/croco/moosngamehaji/MoosnGameHajiApplication.java
@@ -0,0 +1,11 @@
+package me.croco.moosngamehaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoosnGameHajiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MoosnGameHajiApplication.class, args);
+    }
+}
